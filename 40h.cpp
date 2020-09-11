1a8ea113eed9a4ecbc5baeaad14ed17f387d8026
sean echevarria <gh@creepingfog.com>
2020-09-11 16:49:16 -0700

    configure matrix to strip pixel ID for my 5 column layout

--- 40h.cpp
+++ 40h.cpp
@@ -150,17 +150,24 @@ constexpr uint8 kInvalidPixel = -1;
 // map of x/y coordinate to pixel strip ID (x == col, y == row)
 constexpr uint8 kLedMatrix[kMatrixRows][kMatrixCols] 
 {
-    {  0,  1,  2,  3,  4,  5,  6, 7},
-    {  8,  9, 10, 11, 12, 13, 14, 15},
-    { 16, 17, 18, 19, 20, 21, 22, 23 },
-    { 24, 25, 26, 27, 28, 29, kInvalidPixel, kInvalidPixel },
-//     { 32, 33, 34, 35, 36, 37, 38, 39 },
-//     { 40, 41, 42, 43, 44, 45, 46, 47 },
-//     { 48, 49, 50, 51, 52, 53, 54, 55 },
+    // my 5 column config; sparsely maps kLedStripPixelCount to the 8x8 matrix
+    {  0,  1,  2,  3,  4, kInvalidPixel, kInvalidPixel, kInvalidPixel },
+    {  5,  6,  7,  8,  9, kInvalidPixel, kInvalidPixel, kInvalidPixel },
+    { 10, 11, 12, 13, 14, kInvalidPixel, kInvalidPixel, kInvalidPixel },
+    { 15, kInvalidPixel, kInvalidPixel, 16, 17, 18, kInvalidPixel, kInvalidPixel },
     { kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel },
     { kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel },
-    { kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel },
-    { kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel }
+    { 19, 20, 21, 22, 23, 24, 25, 26 }, 
+    { 27, 28, 29, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel, kInvalidPixel }
+
+//  {  0,  1,  2,  3,  4,  5,  6,  7 },
+//  {  8,  9, 10, 11, 12, 13, 14, 15 },
+//  { 16, 17, 18, 19, 20, 21, 22, 23 },
+//  { 24, 25, 26, 27, 28, 29, 30, 31 },
+//  { 32, 33, 34, 35, 36, 37, 38, 39 },
+//  { 40, 41, 42, 43, 44, 45, 46, 47 },
+//  { 48, 49, 50, 51, 52, 53, 54, 55 },
+//  { 56, 57, 58, 59, 60, 61, 62, 63 }
 };
 
 using PixelStrip = NeoPixelBus<NeoRgbFeature, NeoAvr800KbpsMethod>;
